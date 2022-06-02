837c7ce8e5ec8cb6599a2776f9e24ba20b72bd3a
Yuvraj Gupta <yuvraj.gupta@nutanix.com>
2022-06-02 14:56:38 +0530

    Day 2

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  final num day = 3;
+  final String version = "1.0v";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Hello"),
+        title: Text("My Flutter App $version"),
         backgroundColor: Color.fromARGB(255, 13, 13, 14),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to Day $day"),
+        ),
       ),
       drawer: Drawer(),
     );
